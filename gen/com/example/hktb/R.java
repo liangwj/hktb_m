22a1ca31ad9086ba5220fd3ff8daa81b4c4ff76a
lwj <weijian.liang727@gmail.com>
2014-04-24 18:05:25 +0800

    lwj 20140424 登录

--- gen/com/example/hktb/R.java
+++ gen/com/example/hktb/R.java
@@ -14,10 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int btn_log=0x7f070005;
+        public static final int ed_name=0x7f070002;
+        public static final int ed_psw=0x7f070004;
+        public static final int log_name=0x7f070001;
+        public static final int menu_settings=0x7f070006;
+        public static final int textView1=0x7f070000;
+        public static final int textView2=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int login_act=0x7f030001;
+        public static final int workspace_act=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
--- src/com/example/hktb/activity/MainActivity.java
+++ src/com/example/hktb/activity/MainActivity.java
@@ -1,4 +1,8 @@
-package com.example.hktb;
+package com.example.hktb.activity;
+
+import com.example.hktb.R;
+import com.example.hktb.R.layout;
+import com.example.hktb.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -10,13 +14,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
- 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
 	}
 
 }
