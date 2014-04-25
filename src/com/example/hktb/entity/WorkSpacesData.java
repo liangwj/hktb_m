4362ea2772dbdeb2af2cb0f5de768f2fdbdfde93
lwj <weijian.liang727@gmail.com>
2014-04-25 18:18:36 +0800

    listview

---
+++ src/com/example/hktb/entity/WorkSpacesData.java
@@ -0,0 +1,35 @@
+package com.example.hktb.entity;
+
+import java.util.List;
+
+public class WorkSpacesData {
+
+	private String name;
+	private boolean is_support_mobile;
+	private String mobile_menu;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isIs_support_mobile() {
+		return is_support_mobile;
+	}
+
+	public void setIs_support_mobile(boolean is_support_mobile) {
+		this.is_support_mobile = is_support_mobile;
+	}
+
+	public String getMobile_menu() {
+		return mobile_menu;
+	}
+
+	public void setMobile_menu(String mobile_menu) {
+		this.mobile_menu = mobile_menu;
+	}
+
+}
